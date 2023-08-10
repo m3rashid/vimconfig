cbd2e887d898ddf7206f2d118ce384e445c8ac51
m3rashid <m3rashid.hussain@gmail.com>
2023-08-10 10:59:12 +0530

    I am BATMAN

--- lua/user/plugins/community.lua
+++ lua/user/plugins/community.lua
@@ -4,7 +4,10 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+	{ import = "astrocommunity.colorscheme.nightfox-nvim", enabled = false },
+  { import = "astrocommunity.colorscheme.kanagawa-nvim", enabled = true },
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.pack.typescript" },
+	{ import = "astrocommunity.pack.rust" },
 }
